fb0765ec06ae4befadc633c7d0f7529bead8c4dc
qornwh <qornwh@gmail.com>
2024-03-18 12:16:30 +0900

    코드 개선

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,6 +6,7 @@
 #include "pch.h"
 #include "GameSerivce.h"
 #include "IRoom.h"
+#include "SendBuffer.h"
 #include "ThreadManager.h"
 
 int main()
@@ -13,11 +14,10 @@ int main()
     auto count = std::thread::hardware_concurrency() * 2;
     std::cout << "core : " << count << std::endl;
     boost::asio::io_context io_context;
-
     uint16 port = 12128;
 
+    // 스킬 초기화
     GameInit();
-
     // 일반몹 맵 = 0
     GRoomManger->CreateRoom(io_context, 0);
     GRoomManger->getRoom(0)->InitMonsters();
@@ -27,18 +27,36 @@ int main()
 
     GameServiceRef service = boost::make_shared<GameService>(io_context, port);
 
+    if (!service->Start())
+    {
+        // crash!!
+        return 0;
+    }
+
     GThreadManager->CreateThread([&service]()
         {
             service->run();
         }
     );
 
-    if (!service->Start())
-    {
-        // crash!!
-        return 0;
-    }
+    GThreadManager->CreateThread([&service]()
+        {
+            service->run();
+        }
+    );
+
+    GThreadManager->CreateThread([&service]()
+        {
+            service->run();
+        }
+    );
+
+    // 메인스레드 초기화
+    TLS_ThreadId = 0;
+    TLS_SendBufferManager = new SendBufferManager();
     service->run();
+    if (TLS_SendBufferManager != nullptr)
+        delete TLS_SendBufferManager;
 
     return 0;
 }
